18c95b87b656f9d896ffaf1d59f70fb4af31db81
Naveen <57535755+naveen184@users.noreply.github.com>
2023-09-25 18:05:16 +0530

    Update provider.tf

--- .github/workflows/provider.tf
+++ .github/workflows/provider.tf
@@ -1,1 +1,18 @@
+provider "aws"{
+  region = "ap-south-1"
+  access_key = "https://779372627864.signin.aws.amazon.com/console"
+  secret_key = "c!3KE9-|"
+
+
+}
+
+resource "aws-instance" "AWS-ec2-instance" {
+    ami = "ami-008b85aa3ff5c1b02"
+    instance_type = "t2.micro"
+    security_group = ["launch-wizard-1"]
+    key_name = "newpem.file.pem"
+    tags = {
+        Name = "Rd hate server by terraform"
+    }
+}
 
