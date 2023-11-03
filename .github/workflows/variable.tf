1fb31f5fcd472482652567f18d33a989e6bccf3b
Naveen <57535755+naveen184@users.noreply.github.com>
2023-11-03 12:25:51 +0530

    Create variable.tf

---
+++ .github/workflows/variable.tf
@@ -0,0 +1,13 @@
+variable "ami_value" {
+    description = " value for ami"
+  
+}
+variable "instance_type_value" {
+    description = "value for instance_type"
+  
+}
+variable "subnet_id_value" {
+    description = "value for subnet_id"
+  
+}
+  
